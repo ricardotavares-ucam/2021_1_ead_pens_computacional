16602c9ba450072e373f295808e8a993e2009c21
ricardotavares-ucam <ricardo.tavares@ucam-campos.br>
2021-04-11 12:28:15 -0300

    Atualização do material da disciplina

---
+++ Webinar 04 - Revisão/pratica/aula_12-03-2021.py
@@ -0,0 +1,22 @@
+# nota_aluno = 10
+# nota_Aluno = 7.5
+
+# print(nota_aluno is nota_Aluno)
+
+# class = 'Olá Turma 2021 1º período'
+# print(class)
+
+boleana = True
+print(type(boleana))
+
+inteiro = 10
+print(type(inteiro))
+
+n_decimal = 10.0
+print(type(n_decimal))
+
+mensagem_boas_vindas = 'Olá Turma 2021 1º período'
+print(type(mensagem_boas_vindas))
+
+nenhum_valor = None
+print(type(mensagem_boas_vindas))
